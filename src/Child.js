496c6ba14aa25c2a1f65bc526bdc62aab096a1a1
Will <widiep@gmail.com>
2020-01-08 14:15:22 -0800

    Done.

--- src/Child.js
+++ src/Child.js
@@ -3,10 +3,22 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div
+
+      // <div onClick={this.props.handleChangeColor(getRandomColor())}
+
+      // Since we are passing data from the child back up to the parent, we need to change the function call into an anonymous function in order to have React call it once. Otherwise, it would call it definitely/ keep re-rendering reaching a maxium stack exceding
+
+      // We can also put this in a separate method:
+      // handleChangeClick = () => {
+      //   this.props.handleChangeColor(getRandomColor())
+      // }
+
+
+      <div onClick={() =>this.props.handleChangeColor(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
